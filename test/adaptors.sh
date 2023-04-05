4bdc6fee8238e31b417857c47eba6844e2116f55
whlau <raymondlau@live.co.uk>
2023-04-05 10:37:13 +0100

    corrected file name in script

--- test/adaptors.sh
+++ test/adaptors.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # A bash script to extract adaptors from file_list.tsv
-awk '{print $7, $8}' ~/nbaf_2018_raw_data/file_list.tsv > ~/hyRADmod/Test/adaptors.txt # prints column 7 & 8
-tr ' ' '\t' < ~/hyRADmod/Test/adaptors.txt > ~/hyRADmod/Test/adaptors.tmp & mv ~/hyRADmod/Test/adaptors.tmp ~/hyRADmod/Test/adaptors.txt # replaces space with tab
-tr '+' '\t' < ~/hyRADmod/Test/adaptors.txt > ~/hyRADmod/Test/adaptors.tmp & mv ~/hyRADmod/Test/adaptors.tmp ~/hyRADmod/Test/adaptors.txt # replaces + with tab
-tail -n +2 ~/hyRADmod/Test/adaptors.txt > ~/hyRADmod/Test/adaptors.tmp & mv ~/hyRADmod/Test/adaptors.tmp ~/hyRADmod/Test/adaptors.txt # removes top line
-awk '!a[$0]++' ~/hyRADmod/Test/adaptors.txt > ~/hyRADmod/Test/adaptors.tmp && mv ~/hyRADmod/Test/adaptors.tmp ~/hyRADmod/Test/adaptors.txt # removes duplicate lines
-awk '{sub("$", "T", $2)}; 1' ~/hyRADmod/Test/adaptors.txt > ~/hyRADmod/Test/adaptors.tmp & mv ~/hyRADmod/Test/adaptors.tmp ~/hyRADmod/Test/adaptors.txt #Adds a T to the end of every sample name
-tr ' ' '\t' < ~/hyRADmod/Test/adaptors.txt > ~/hyRADmod/Test/adaptors.tmp & mv ~/hyRADmod/Test/adaptors.tmp ~/hyRADmod/Test/adaptors.txt # replaces space with tab as previous action replaced tab with space
-cat -T ~/hyRADmod/Test/adaptors.txt # prints file and shows Tabs as ^I
+awk '{print $7, $8}' ~/nbaf_2018_raw_data/file_list.tsv > ~/hyRADmod/test/adaptors.txt # prints column 7 & 8
+tr ' ' '\t' < ~/hyRADmod/test/adaptors.txt > ~/hyRADmod/test/adaptors.tmp & mv ~/hyRADmod/test/adaptors.tmp ~/hyRADmod/test/adaptors.txt # replaces space with tab
+tr '+' '\t' < ~/hyRADmod/test/adaptors.txt > ~/hyRADmod/test/adaptors.tmp & mv ~/hyRADmod/test/adaptors.tmp ~/hyRADmod/test/adaptors.txt # replaces + with tab
+tail -n +2 ~/hyRADmod/test/adaptors.txt > ~/hyRADmod/test/adaptors.tmp & mv ~/hyRADmod/test/adaptors.tmp ~/hyRADmod/test/adaptors.txt # removes top line
+awk '!a[$0]++' ~/hyRADmod/test/adaptors.txt > ~/hyRADmod/test/adaptors.tmp && mv ~/hyRADmod/test/adaptors.tmp ~/hyRADmod/test/adaptors.txt # removes duplicate lines
+awk '{sub("$", "T", $2)}; 1' ~/hyRADmod/test/adaptors.txt > ~/hyRADmod/test/adaptors.tmp & mv ~/hyRADmod/test/adaptors.tmp ~/hyRADmod/test/adaptors.txt #Adds a T to the end of every sample name
+tr ' ' '\t' < ~/hyRADmod/test/adaptors.txt > ~/hyRADmod/test/adaptors.tmp & mv ~/hyRADmod/test/adaptors.tmp ~/hyRADmod/test/adaptors.txt # replaces space with tab as previous action replaced tab with space
+cat -T ~/hyRADmod/test/adaptors.txt # prints file and shows Tabs as ^I
